83f4f63983cb8f073ea668cab1be436ba8f4f13c
AllenKaplan <allenkaplan07@gmail.com>
2022-03-04 18:21:42 -0500

    parsing form

--- api/server.go
+++ api/server.go
@@ -3,7 +3,7 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
-	"io/ioutil"
+	"time"
 )
 
 type VerifiServer interface {
@@ -24,7 +24,24 @@ func (s Server) FormSubmittedWebhook(c *gin.Context) {
 	s.logger.Infow("Received Form Submission")
 	c.JSON(200, gin.H{"message": "form sent to server"})
 
+	type Request struct {
+		FormId       string    `json:"form_id"`
+		FormName     string    `json:"form_name"`
+		FormUrl      string    `json:"form_url"`
+		ResponseDate time.Time `json:"response_date"`
+		ResponseId   string    `json:"response_id"`
+		Email        string    `json:"email"`
+		FirstName    string    `json:"first_name"`
+		LastName     string    `json:"last_name"`
+		Pronouns     string    `json:"pronouns"`
+		Major        string    `json:"major"`
+		Affiliation  string    `json:"affiliation"`
+		DiscordName  string    `json:"discord_name"`
+		DiscordId    string    `json:"discord_id"`
+	}
 
-	body, _ := ioutil.ReadAll(c.Request.Body)
-	s.logger.Debugw("printing body", "body", body)
+	var request Request
+	c.Bind(&request)
+
+	s.logger.Debugw("parsed request", "request", request)
 }
